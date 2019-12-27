641540eff6aa5f00650e558ff892457bcece4929
nileshkalal <59273557+nileshkalal@users.noreply.github.com>
2019-12-27 10:47:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<?php
-
-
-echo "Welcome Nilesh";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo</title>
+</head>
+<body>
+<h1>Hi Nilesh</h1>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Salman_Khan_at_Renault_Star_Guild_Awards.jpg/220px-Salman_Khan_at_Renault_Star_Guild_Awards.jpg">
+</body>
+</html>
